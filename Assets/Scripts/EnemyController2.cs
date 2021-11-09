db09efd10332e9f790f759cf5fcef2c4d652eaa0
StarsZakata <sergeizerro@mail.ru>
2021-11-09 21:59:14 +0300

    End

---
+++ Assets/Scripts/EnemyController2.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController2 : MonoBehaviour
+{
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
+    Animator animator;
+    // Start is called before the first frame update
+
+    bool broken = true;
+
+    public ParticleSystem smokeEffects;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+        Vector2 position = rigidbody2D.position;
+
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move x", 0);
+            animator.SetFloat("Move y", direction);
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move x", direction);
+            animator.SetFloat("Move y", 0);
+        }
+        rigidbody2D.MovePosition(position);
+
+
+        if (!broken)
+        {
+            return;
+        }
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+        if (!broken)
+        {
+            return;
+        }
+    }
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        animator.SetTrigger("Fixed");
+        smokeEffects.Stop();
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rigidbody2d;
+
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (transform.position.magnitude > 10.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Направление и Сила Снаряда
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+    }
+
+    //попадение снаряда
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        EnemyController2 e = other.collider.GetComponent<EnemyController2>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+
+        Destroy(gameObject);
+    }
+}
